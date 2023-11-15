bb8be142f51bcf76597b50f44f95bd4a21de8b9a
Evan Doxanis <evan.doxanis@etu.univ-lyon1.fr>
2023-11-15 15:43:50 +0100

    Update Grid.java

--- src/model/grid/Grid.java
+++ src/model/grid/Grid.java
@@ -1,19 +1,47 @@
 package model.grid;
+import model.block.Block;
+import model.block.BlockType;
+import model.piece.Piece;
 
 public class Grid {
     private int lenght;
     private int width;
-    private  Case[][] grd;
+    private  Block[][] grd;
 
     public Grid(int lenght, int width){
         this.lenght = lenght;
         this.width = width;
-        grd = new Case[lenght][width];
+        grd = new Block[lenght][width];
 
         for(int i=0;i < lenght; i++){
             for(int j=0;j < width; j++){
-                grd[i][j] = new Case();
+                grd[i][j] = new Block();
             }
         }
+    }
+
+    public boolean CanGoDown(Piece p){
+        Block[][][] piece;
+        piece = p.getBlock;
+        for (int i = 0; i < piece.length; i++) {
+            for (int j = 0; j < piece[i].length; j++) {
+                for (int k = 0; k < piece[i][j].length; k++) {
+
+                    if(piece[i][j][k].getType() != BlockType.EMPTY &&
+                            grd[p.getX()+j][p.getY()+k].getType() != BlockType.EMPTY){
+                        return false;
+                    }
+
+                }
+            }
+        }
+    }
+
+    public boolean CanGoLeft(){
+
+    }
+
+    public boolean CanGoRight(){
+
     }
 }
