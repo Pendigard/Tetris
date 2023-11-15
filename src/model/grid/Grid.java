d896cf58a2c2c4777f112f67f846f4f287ddee90
Evan Doxanis <evan.doxanis@etu.univ-lyon1.fr>
2023-11-15 17:10:40 +0100

    putPiece, checkLine, Position function

--- src/model/grid/Grid.java
+++ src/model/grid/Grid.java
@@ -34,14 +34,14 @@ public class Grid {
         for (int i = 0; i < piece.length; i++) {
             for (int j = 0; j < piece[i].length; j++) {
 
-                if (piece[i][j].getType() != BlockType.EMPTY &&
-                        grd[p.getX() + i][p.getY() + j+1].getType() != BlockType.EMPTY) {
+                if ( grd[0].length >= p.getY()+j+1 || (piece[i][j].getType() != BlockType.EMPTY &&
+                        grd[p.getX() + i][p.getY() + j+1].getType() != BlockType.EMPTY) ){
                     return false;
                 }
 
             }
         }
-        return false;
+        return true;
     }
 
     public boolean CanGoLeft(Piece p) {
@@ -49,7 +49,6 @@ public class Grid {
 
         for (int i = 0; i < piece.length; i++) {
             for (int j = 0; j < piece[i].length; j++) {
-
                 if (piece[i][j].getType() != BlockType.EMPTY &&
                         grd[p.getX() + i-1][p.getY() + j].getType() != BlockType.EMPTY) {
                     return false;
@@ -76,20 +75,23 @@ public class Grid {
         return true;
     }
 
-    public boolean CanRotate(Piece p) {
-        Block[][] piece = p.getBlocks();
+    public int CanRotate(Piece p) {
+        for(int k=0;k<4;k++) {
+            p.rotate();
+            Block[][] piece = p.getBlocks();
 
-        for (int i = 0; i < piece.length; i++) {
-            for (int j = 0; j < piece[i].length; j++) {
+            for (int i = 0; i < piece.length; i++) {
+                for (int j = 0; j < piece[i].length; j++) {
 
-                if (piece[i][j].getType() != BlockType.EMPTY &&
-                        grd[p.getX() + i][p.getY() + j].getType() != BlockType.EMPTY) {
-                    return false;
+                    if (piece[i][j].getType() != BlockType.EMPTY &&
+                            grd[p.getX() + i][p.getY() + j].getType() == BlockType.EMPTY) {
+                        return p.getRotation();
+                    }
+
                 }
-
             }
         }
-        return true;
+        return -1;
     }
 
     public void putPiece(Piece p) {
@@ -100,7 +102,6 @@ public class Grid {
 
                 if (piece[i][j].getType() != BlockType.EMPTY){
                         grd[p.getX() + i][p.getY() + j].setType(piece[i][j].getType());
-
                 }
 
             }
@@ -108,6 +109,17 @@ public class Grid {
     }
 
     public int checkLine(Piece p){
-        return 3;
+        int result=0;
+        for(int i=grd[0].length;i>0;i--){
+            boolean full = true;
+            for(int j=0;j<grd.length;j++){
+                if (grd[i][j].getType() == BlockType.EMPTY) {
+                    full = false;
+                    break;
+                }
+            }
+            if(full) result++;
+        }
+        return result;
     }
 }
